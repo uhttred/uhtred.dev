eca19e76fa19ba1dca1aaac273e8de8ed7442c10
Uhtred M <am@uhtred.dev>
2023-10-11 06:32:08 +0100

    terms updated

--- src/views/help/privacy-policy.vue
+++ src/views/help/privacy-policy.vue
@@ -1,13 +1,13 @@
 <template>
   <article class="pt-5.5 lg:pl-4 pb-12 min-h-[600px]">
-    <UhMarkdown
-      v-if="locale === 'pt'"
-      :content="pt_data"
-    />
-    <UhMarkdown
-      v-else
-      :content="en_data"
-    />
+<UhMarkdown
+  v-if="locale === 'pt'"
+  :content="pt_data"
+/>
+<UhMarkdown
+  v-else
+  :content="en_data"
+/>
   </article>
 </template>
 
@@ -20,8 +20,8 @@ definePageMeta({
 })
 useSchemaOrg([
   defineWebPage({
-    description: t('pages.help.privacy-policy.description'),
-    name: t('pages.help.privacy-policy.title')
+description: t('pages.help.privacy-policy.description'),
+name: t('pages.help.privacy-policy.title')
   })
 ])
 
@@ -33,66 +33,68 @@ _Última atualização: 24/9/2023_
 Sua privacidade é importante para nós. Esta Política de Privacidade e Dados explica como Uhtred M. (referido como "nós", "nosso" ou "nos") coleta, usa e protege suas informações pessoais quando você usa nosso site (referido como o "Site"). Ao acessar ou utilizar o Site, você concorda com as práticas descritas nesta política. Se você não concorda com esta política, por favor não utilize o Site.
 
 1. **Informações que coletamos:**
-    - **Informações Pessoais:** Poderemos coletar informações pessoais fornecidas voluntariamente por você, como seu endereço de e-mail quando você assina nossa newsletter. Não coletamos nenhuma informação pessoal obrigatória.
-    - **Informações Não Pessoais:** Também poderemos coletar informações não pessoais, como seu endereço IP, tipo de navegador e sistema operacional, para melhorar sua experiência em nosso Site.
+- **Informações Pessoais:** Poderemos coletar informações pessoais fornecidas voluntariamente por você, como seu endereço de e-mail quando você assina nossa newsletter. Não coletamos nenhuma informação pessoal obrigatória.
+- **Informações Não Pessoais:** Também poderemos coletar informações não pessoais, como seu endereço IP, tipo de navegador e sistema operacional, para melhorar sua experiência em nosso Site.
+- **Google Signals:** podemos usar os Google Signals no Google Analytics para permitir a coleta de dados adicionais de usuários que ativaram a Personalização de anúncios em suas contas do Google. Esses dados são agregados e anonimizados para fornecer insights sobre o comportamento do usuário em todos os dispositivos.
 
 2. **Uso de informações:**
-    - **Informações Pessoais:** Poderemos usar seu endereço de e-mail para enviar boletins informativos periódicos e atualizações sobre nosso conteúdo, produtos ou serviços. Você pode cancelar a assinatura dessas comunicações a qualquer momento.
-    - **Informações Não Pessoais:** Informações não pessoais são usadas para análise e para melhorar sua experiência em nosso Site. Isso nos ajuda a entender como os usuários interagem com nosso conteúdo e a melhorar nossos serviços.
+- **Informações Pessoais:** Poderemos usar seu endereço de e-mail para enviar boletins informativos periódicos e atualizações sobre nosso conteúdo, produtos ou serviços. Você pode cancelar a assinatura dessas comunicações a qualquer momento.
+- **Informações Não Pessoais:** Informações não pessoais são usadas para análise e para melhorar sua experiência em nosso Site. Isso nos ajuda a entender como os usuários interagem com nosso conteúdo e a melhorar nossos serviços.
 
 3. **Biscoitos:**
-    - Utilizamos cookies para melhorar a experiência do usuário e coletar informações não pessoais. Você pode gerenciar ou desativar os cookies através das configurações do seu navegador. No entanto, observe que alguns recursos do Site podem não funcionar corretamente sem cookies. Para saber mais sobre como usamos cookies, acesse a seção de cookies.
+- Utilizamos cookies para melhorar a experiência do usuário e coletar informações não pessoais. Você pode gerenciar ou desativar os cookies através das configurações do seu navegador. No entanto, observe que alguns recursos do Site podem não funcionar corretamente sem cookies. Para saber mais sobre como usamos cookies, acesse a seção de cookies.
 
 4. **Links de terceiros:**
-    - Nosso Site pode conter links para sites ou serviços externos que não são operados por nós. Não somos responsáveis pelas práticas de privacidade ou pelo conteúdo desses sites de terceiros. Encorajamos você a revisar suas respectivas políticas de privacidade.
+- Nosso Site pode conter links para sites ou serviços externos que não são operados por nós. Não somos responsáveis pelas práticas de privacidade ou pelo conteúdo desses sites de terceiros. Encorajamos você a revisar suas respectivas políticas de privacidade.
 
 5. **Segurança de dados:**
-    - Tomamos medidas razoáveis para proteger suas informações pessoais. No entanto, esteja ciente de que nenhum método de transmissão de dados pela Internet é completamente seguro e não podemos garantir a segurança das suas informações.
+- Tomamos medidas razoáveis para proteger suas informações pessoais. No entanto, esteja ciente de que nenhum método de transmissão de dados pela Internet é completamente seguro e não podemos garantir a segurança das suas informações.
 
 6. **Privacidade das Crianças:**
-    - Nosso site não se destina a indivíduos com menos de 13 anos. Não coletamos intencionalmente informações pessoais de crianças menores de 13 anos. Se você acredita que podemos ter coletado tais informações, entre em contato conosco e tomaremos imediatamente as medidas apropriadas.
+- Nosso site não se destina a indivíduos com menos de 13 anos. Não coletamos intencionalmente informações pessoais de crianças menores de 13 anos. Se você acredita que podemos ter coletado tais informações, entre em contato conosco e tomaremos imediatamente as medidas apropriadas.
 
 7. **Alterações nesta Política:**
-    - Poderemos atualizar esta Política de Privacidade e Dados periodicamente para refletir alterações em nossas práticas ou por motivos legais ou regulatórios. A data da última revisão será indicada no início da apólice.
+- Poderemos atualizar esta Política de Privacidade e Dados periodicamente para refletir alterações em nossas práticas ou por motivos legais ou regulatórios. A data da última revisão será indicada no início da apólice.
 
 8. **Informações de contato:**
-    - Se tiver alguma dúvida ou preocupação sobre esta Política de Privacidade e Dados, ou se desejar exercer os seus direitos relativamente às suas informações pessoais, por favor aceda à [página de contactos](https://uhtred.dev/help/contact-us).
+- Se tiver alguma dúvida ou preocupação sobre esta Política de Privacidade e Dados, ou se desejar exercer os seus direitos relativamente às suas informações pessoais, por favor aceda à [página de contactos](https://uhtred.dev/help/contact-us).
 
-Ao usar o site Uhtred M., você reconhece e concorda com os termos desta Política de Privacidade e Dados. Obrigado por visitar nosso site e nos confiar suas informações. Sua privacidade é importante para nós e estamos comprometidos em protegê-la.
+Ao usar o site Uhtred M, você reconhece e concorda com os termos desta Política de Privacidade e Dados. Obrigado por visitar nosso site e nos confiar suas informações. Sua privacidade é importante para nós e temos o compromisso de protegê-la, ao mesmo tempo em que aderimos aos princípios dos Sinais do Google, conforme descritos em suas políticas oficiais. Para garantir a conformidade com as políticas mais recentes do Google, consulte a documentação oficial do Google e procure aconselhamento jurídico, se necessário.
 `
 const en_data = `
 # Privacy and Data Policy
 
-_Last updated: 9/24/2023_
+_Last updated: 10/10/2023_
 
-Your privacy is important to us. This Privacy and Data Policy explains how Uhtred M (referred to as "we," "our," or "us") collects, uses, and protects your personal information when you use our website (referred to as the "Website"). By accessing or using the Website, you consent to the practices described in this policy. If you do not agree with this policy, please do not use the Website.
+Your privacy is essential to us. This Privacy and Data Policy outlines how Uhtred M (referred to as "we," "our," or "us") collects, uses, and protects your personal information while using our website (referred to as the "Website"). By accessing or using the Website, you consent to the practices described in this policy. If you do not agree with this policy, please refrain from using the Website.
 
 1. **Information We Collect:**
-    - **Personal Information:** We may collect personal information voluntarily provided by you, such as your email address when you subscribe to our newsletter. We do not collect any mandatory personal information.
-    - **Non-Personal Information:** We may also collect non-personal information, such as your IP address, browser type, and operating system, to improve your experience on our Website.
+- **Personal Information:** We may collect personal information voluntarily provided by you, such as your email address when you subscribe to our newsletter. We do not collect any mandatory personal information.
+- **Non-Personal Information:** We may also collect non-personal information, such as your IP address, browser type, and operating system, to improve your experience on our Website.
+- **Google Signals:** We may use Google Signals in Google Analytics to enable additional data collection from users who have turned on Ad Personalization in their Google accounts. This data is aggregated and anonymized to provide insights into user behavior across devices.
 
 2. **Use of Information:**
-    - **Personal Information:** We may use your email address to send you periodic newsletters and updates about our content, products, or services. You can unsubscribe from these communications at any time.
-    - **Non-Personal Information:** Non-personal information is used for analytics and to enhance your experience on our Website. It helps us understand how users interact with our content and improve our services.
+- **Personal Information:** We may use your email address to send you periodic newsletters and updates about our content, products, or services. You can unsubscribe from these communications at any time.
+- **Non-Personal Information:** Non-personal information is used for analytics and to enhance your experience on our Website. It helps us understand how users interact with our content and improve our services.
 
 3. **Cookies:**
-    - We use cookies to enhance user experience and collect non-personal information. You can manage or disable cookies through your browser settings. However, please note that certain features of the Website may not function properly without cookies. To know more about how we use cookie, please go to [cookie section](https://uhtred.dev/help/cookie-policy).
+- We use cookies to enhance user experience and collect non-personal information. You can manage or disable cookies through your browser settings. However, please note that certain features of the Website may not function properly without cookies. To know more about how we use cookie, please go to [cookie section](https://uhtred.dev/help/cookie-policy).
 
 4. **Third-Party Links:**
-    - Our Website may contain links to external websites or services that are not operated by us. We are not responsible for the privacy practices or content of these third-party websites. We encourage you to review their respective privacy policies.
+- Our Website may contain links to external websites or services that are not operated by us. We are not responsible for the privacy practices or content of these third-party websites. We encourage you to review their respective privacy policies.
 
 5. **Data Security:**
-    - We take reasonable measures to protect your personal information. However, please be aware that no method of data transmission over the internet is completely secure, and we cannot guarantee the security of your information.
+- We take reasonable measures to protect your personal information. However, please be aware that no method of data transmission over the internet is completely secure, and we cannot guarantee the security of your information.
 
 6. **Children's Privacy:**
-    - Our Website is not intended for individuals under the age of 13. We do not knowingly collect personal information from children under 13. If you believe that we may have collected such information, please contact us, and we will promptly take appropriate action.
+- Our Website is not intended for individuals under the age of 13. We do not knowingly collect personal information from children under 13. If you believe that we may have collected such information, please contact us, and we will promptly take appropriate action.
 
 7. **Changes to this Policy:**
-    - We may update this Privacy and Data Policy from time to time to reflect changes in our practices or for legal or regulatory reasons. The date of the latest revision will be indicated at the beginning of the policy.
+- We may update this Privacy and Data Policy from time to time to reflect changes in our practices or for legal or regulatory reasons. The date of the latest revision will be indicated at the beginning of the policy.
 
 8. **Contact Information:**
-    - If you have any questions or concerns about this Privacy and Data Policy, or if you wish to exercise your rights regarding your personal information, please go to [contact details](https://uhtred.dev/help/contact-us) page.
+- If you have any questions or concerns about this Privacy and Data Policy, or if you wish to exercise your rights regarding your personal information, please go to [contact details](https://uhtred.dev/help/contact-us) page.
 
-By using the Uhtred M. Website, you acknowledge and agree to the terms of this Privacy and Data Policy. Thank you for visiting our Website and entrusting us with your information. Your privacy is important to us, and we are committed to safeguarding it.
+By using the Uhtred M Website, you acknowledge and agree to the terms of this Privacy and Data Policy. Thank you for visiting our Website and entrusting us with your information. Your privacy is important to us, and we are committed to safeguarding it while adhering to the principles of Google Signals as described in their official policies. To ensure compliance with the latest Google policies, please consult Google's official documentation and seek legal advice if necessary.
 `
 </script>
--- src/views/help/terms.vue
+++ src/views/help/terms.vue
@@ -28,91 +28,91 @@ useSchemaOrg([
 const pt_data = `
 # Termos de uso
 
-_Última atualização: 24/9/2023_
+_Última atualização: 10/10/2023_
 
 Bem-vindo ao Uhtred M. (referido como "Site" ou "Site"). Por favor, leia estes Termos de Uso (“Termos”) cuidadosamente antes de usar este site. Ao acessar ou usar este Site, você concorda em cumprir e ficar vinculado a estes Termos. Se você não concorda com estes Termos, por favor não use este Site.
 
 1. **Aceitação dos Termos:** Ao acessar ou usar o site Uhtred M., você concorda em ficar legalmente vinculado a estes Termos. Se você não concordar com estes Termos, evite usar o Site.
 
 2. **Uso do Site:**
-    - Este site destina-se apenas a fins pessoais e informativos.
-    - Você pode usar o site para navegar, acessar artigos, visualizar trabalhos apresentados e entrar em contato com Uhtred M. para solicitações de serviço.
-    - Você pode assinar nossa newsletter voluntariamente, fornecendo seu endereço de e-mail.
-    - Você não pode usar o Site para fins ilegais, maliciosos ou prejudiciais.
+- Este site destina-se apenas a fins pessoais e informativos.
+- Você pode usar o site para navegar, acessar artigos, visualizar trabalhos apresentados e entrar em contato com Uhtred M. para solicitações de serviço.
+- Você pode assinar nossa newsletter voluntariamente, fornecendo seu endereço de e-mail.
+- Você não pode usar o Site para fins ilegais, maliciosos ou prejudiciais.
 
 3. **Artigos e Conteúdo:**
-    - Os artigos publicados neste Site têm caráter meramente informativo e não constituem aconselhamento profissional. O programador e os parceiros fornecem conteúdo a seu critério e nem sempre refletem as informações mais atuais.
-    - Os visitantes são incentivados a verificar as informações de forma independente e a procurar aconselhamento profissional conforme necessário.
+- Os artigos publicados neste Site têm caráter meramente informativo e não constituem aconselhamento profissional. O programador e os parceiros fornecem conteúdo a seu critério e nem sempre refletem as informações mais atuais.
+- Os visitantes são incentivados a verificar as informações de forma independente e a procurar aconselhamento profissional conforme necessário.
 
 4. **Produtos Digitais:**
-    - O Site poderá oferecer produtos digitais para venda, mas a compra desses produtos será concretizada em plataformas ou sites externos. Não nos responsabilizamos por transações ou questões relacionadas a plataformas externas.
-    - Quaisquer termos e condições relacionados com a compra de produtos digitais estão sujeitos às políticas das respetivas plataformas externas.
+- O Site poderá oferecer produtos digitais para venda, mas a compra desses produtos será concretizada em plataformas ou sites externos. Não nos responsabilizamos por transações ou questões relacionadas a plataformas externas.
+- Quaisquer termos e condições relacionados com a compra de produtos digitais estão sujeitos às políticas das respetivas plataformas externas.
 
 5. **Privacidade e Cookies:**
-    - Nós respeitamos sua privacidade. As informações coletadas neste Site, como seu endereço de e-mail para assinatura de boletins informativos, serão tratadas de acordo com nossa Política de Privacidade.
-    - Este site utiliza cookies para melhorar a experiência do usuário. Ao utilizar o Site, você concorda com o uso de cookies de acordo com nossa Política de Cookies.
+- Nós respeitamos sua privacidade. As informações coletadas neste Site, como seu endereço de e-mail para assinatura de boletins informativos, serão tratadas de acordo com nossa Política de Privacidade. Também podemos usar Google Signals dentro do Google Analytics para coleta de dados.
+- Este site utiliza cookies para melhorar a experiência do usuário. Ao utilizar o Site, você concorda com o uso de cookies de acordo com nossa Política de Cookies.
 
 6. **Assinatura do boletim informativo (newsletter):**
-    - Ao assinar nossa newsletter, você concorda em receber nossas atualizações por e-mail. Você pode cancelar a inscrição a qualquer momento seguindo as instruções fornecidas em nossos e-mails.
+- Ao assinar nossa newsletter, você concorda em receber nossas atualizações por e-mail. Você pode cancelar a inscrição a qualquer momento seguindo as instruções fornecidas em nossos e-mails.
 
 7. **Propriedade Intelectual:**
-    - Todo o conteúdo, incluindo artigos, imagens e trabalhos apresentados, neste site é propriedade intelectual de Uhtred M. e não pode ser reproduzido, distribuído ou usado sem permissão explícita.
+- Todo o conteúdo, incluindo artigos, imagens e trabalhos apresentados, neste site é propriedade intelectual de Uhtred M. e não pode ser reproduzido, distribuído ou usado sem permissão explícita.
 
 8. **Limitação de Responsabilidade:**
-    - Não oferecemos garantias ou representações sobre a exatidão ou integridade do conteúdo deste site.
-    - Não nos responsabilizamos por quaisquer danos diretos ou indiretos decorrentes do uso deste Site ou do conteúdo aqui fornecido.
+- Não oferecemos garantias ou representações sobre a exatidão ou integridade do conteúdo deste site.
+- Não nos responsabilizamos por quaisquer danos diretos ou indiretos decorrentes do uso deste Site ou do conteúdo aqui fornecido.
 
 9. **Alterações nos Termos:**
-    - Poderemos atualizar estes Termos a qualquer momento. Por favor, verifique esta página periodicamente para mudanças. O uso continuado do Site após quaisquer modificações indica sua aceitação dos Termos atualizados.
+- Poderemos atualizar estes Termos a qualquer momento. Por favor, verifique esta página periodicamente para mudanças. O uso continuado do Site após quaisquer modificações indica sua aceitação dos Termos atualizados.
 
 10. **Informações de contato:**
-     - Para qualquer dúvida ou preocupação em relação a estes Termos de Uso, acesse a página de [detalhes de contato](https://uhtred.dev/pt/help/contact-us).
+- Para qualquer dúvida ou preocupação em relação a estes Termos de Uso, acesse a página de [detalhes de contato](https://uhtred.dev/pt/help/contact-us).
 
-Obrigado por visitar Uhtred M. Aproveite sua experiência em nosso site!
+Obrigado por visitar Uhtred M. Aproveite sua experiência em nosso site e tenha certeza de que aderimos aos princípios do Google Signals para coleta de dados e privacidade de acordo com nossa Política de Privacidade e Dados. Para garantir a conformidade com as políticas mais recentes do Google, consulte a documentação oficial do Google e procure aconselhamento jurídico, se necessário.
 `
 const en_data = `
 # Terms of Use
 
-_Last Updated: 9/24/2023_
+_Last Updated: 10/10/2023_
 
 Welcome to Uhtred M. (referred to as "Website" or "Site"). Please read these Terms of Use ("Terms") carefully before using this website. By accessing or using this Site, you agree to comply with and be bound by these Terms. If you do not agree with these Terms, please do not use this Website.
 
 1. **Acceptance of Terms:** By accessing or using the Uhtred M. Website, you agree to be legally bound by these Terms. If you do not agree to these Terms, please refrain from using the Website.
 
 2. **Use of the Website:**
-    - This Website is intended for personal and informational purposes only.
-    - You may use the Website to browse, access articles, view showcased work, and contact Uhtred M. for service requests.
-    - You may subscribe to our newsletter voluntarily by providing your email address.
-    - You may not use the Website for any unlawful, malicious, or harmful purposes.
+- This Website is intended for personal and informational purposes only.
+- You may use the Website to browse, access articles, view showcased work, and contact Uhtred M. for service requests.
+- You may subscribe to our newsletter voluntarily by providing your email address.
+- You may not use the Website for any unlawful, malicious, or harmful purposes.
 
 3. **Articles and Content:**
-   - The articles published on this Website are for informational purposes only and do not constitute professional advice. The programmer and partners provide content at their discretion, and it may not always reflect the most current information.
-   - Visitors are encouraged to verify information independently and seek professional advice as needed.
+- The articles published on this Website are for informational purposes only and do not constitute professional advice. The programmer and partners provide content at their discretion, and it may not always reflect the most current information.
+- Visitors are encouraged to verify information independently and seek professional advice as needed.
 
 4. **Digital Products:**
-   - The Website may offer digital products for sale, but the purchase of these products will be completed on external platforms or websites. We are not responsible for transactions or issues related to external platforms.
-   - Any terms and conditions related to the purchase of digital products are subject to the policies of the respective external platforms.
+- The Website may offer digital products for sale, but the purchase of these products will be completed on external platforms or websites. We are not responsible for transactions or issues related to external platforms.
+- Any terms and conditions related to the purchase of digital products are subject to the policies of the respective external platforms.
 
 5. **Privacy and Cookies:**
-   - We respect your privacy. Information collected on this Website, such as your email address for newsletter subscriptions, will be handled in accordance with our Privacy Policy.
-   - This Website uses cookies to enhance user experience. By using the Website, you consent to the use of cookies in accordance with our Cookie Policy.
+- We respect your privacy. Information collected on this Website, such as your email address for newsletter subscriptions, will be handled in accordance with our Privacy Policy. We may also use Google Signals within Google Analytics for data collection.
+- This Website uses cookies to enhance user experience. By using the Website, you consent to the use of cookies in accordance with our Cookie Policy.
 
 6. **Newsletter Subscription:**
-   - By subscribing to our newsletter, you consent to receive email updates from us. You can unsubscribe at any time by following the instructions provided in our emails.
+- By subscribing to our newsletter, you consent to receive email updates from us. You can unsubscribe at any time by following the instructions provided in our emails.
 
 7. **Intellectual Property:**
-   - All content, including articles, images, and showcased work, on this Website is the intellectual property of Uhtred M and may not be reproduced, distributed, or used without explicit permission.
+- All content, including articles, images, and showcased work, on this Website is the intellectual property of Uhtred M and may not be reproduced, distributed, or used without explicit permission.
 
 8. **Limitation of Liability:**
-    - We make no warranties or representations about the accuracy or completeness of the content on this Website.
-    - We are not liable for any direct or indirect damages arising from your use of this Website or the content provided herein.
+- We make no warranties or representations about the accuracy or completeness of the content on this Website.
+- We are not liable for any direct or indirect damages arising from your use of this Website or the content provided herein.
 
 9. **Changes to Terms:**
-    - We may update these Terms at any time. Please check this page periodically for changes. Your continued use of the Website after any modifications indicates your acceptance of the updated Terms.
+- We may update these Terms at any time. Please check this page periodically for changes. Your continued use of the Website after any modifications indicates your acceptance of the updated Terms.
 
 10. **Contact Information:**
-    - For any questions or concerns regarding these Terms of Use, please go to [contact details](https://uhtred.dev/help/contact-us) page.
+- For any questions or concerns regarding these Terms of Use, please go to [contact details](https://uhtred.dev/help/contact-us) page.
 
-Thank you for visiting Uhtred M. Enjoy your experience on our Website!
+Thank you for visiting Uhtred M. Enjoy your experience on our Website, and rest assured that we adhere to Google Signals principles for data collection and privacy in accordance with our Privacy and Data Policy. To ensure compliance with the latest Google policies, please consult Google's official documentation and seek legal advice if necessary.
 `
 </script>
